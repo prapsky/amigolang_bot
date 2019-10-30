8d60ca66d97c0d0fc2577bdadd6bc2e25f7861b8
praptobl <suprapto@bukalapak.com>
2019-10-30 23:13:46 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/prapsky/amigolang-bot
+
+go 1.12
+
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4 // indirect
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "log"
+  "os"
+  tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+  var (
+    port = os.Getenv("PORT")
+    publicURL = os.Getenv("PUBLIC_URL")
+    token = os.Getenv("TOKEN")
+  )
+
+  webhook := &tb.Webhook{
+    Listen: ":" + port,
+    Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+  }
+
+  pref := tb.Settings{
+    Token: token,
+    Poller: webhook,
+  }
+
+  b, err := tb.NewBot(pref)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  b.Handle("/hello", func(m *tb.Message)) {
+    b.Send(m.Sender, "Hi!")
+  }
+}
